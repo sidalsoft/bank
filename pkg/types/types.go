2ccaaa2316a98333b8395aab8bbe0bf20768a3e1
Saidali Saburov <ssaburov@velox.biz>
2021-04-02 23:54:11 +0500

    -update types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -2,38 +2,20 @@ package types
 
 type Money int64
 
-type Currency string
+type Category string
+
+type Status string
 
 const (
-	TJS Currency = "TJS"
-	RUB Currency = "RUB"
-	USD Currency = "USD"
-	EUR Currency = "EUR"
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
 )
-
-type PAN string
-
-type Card struct {
-	Id       int
-	PAN      PAN
-	Balance  Money
-	Currency Currency
-	Color    string
-	Name     string
-	Active   bool
-}
-
-type Category string
 
 // Payment provides information about the payment
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
-}
-
-type PaymentSource struct {
-	Type    string // 'card'
-	Number  string // номер вида '5058 xxxx xxxx 8888'
-	Balance Money  // баланс в дирамах
+	Status   Status
 }
